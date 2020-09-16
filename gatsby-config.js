2d7f60ecf39cf80e0070e669bc380ee66ea01706
Eric Dumas <edumas000@gmail.com>
2020-09-16 01:17:24 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,37 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Eric Dumas",
+    description: "I teach JavaScript, React, GraphQL and Gatsby",
+    author: "Eric Dumas",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+import Metadata from "../components/metadata"
+
+const Contact = () => {
+  return (
+    <Layout>
+    <Metadata title="Contact" description="This is my home page" /> 
+      <div>
+      <Metadata title="Contact" description="This is my home page" />
+        <h1>Contact Page</h1>
+        <p>You can find Ibas on Twitter via @ibaslogic</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default Contact
